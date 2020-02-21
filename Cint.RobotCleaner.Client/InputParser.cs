437f5e35e6d1d4cf470fdf3415f6e0f365da2199
Eugene Rebedailo <mokonamodokidesu@gmail.com>
2020-02-21 23:46:07 +0300

    Added input parser

---
+++ Cint.RobotCleaner.Client/InputParser.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Cint.RobotCleaner.Core.Impl;
+
+namespace Cint.RobotCleaner.Client
+{
+    public class InputParser
+    {
+        private TextReader reader;
+
+        public InputParser(Stream input)
+        {
+            reader = new StreamReader(input);
+        }
+
+        private int? commandCount;
+
+        /// <summary>
+        /// The amount of commands to execute
+        /// </summary>
+        public int CommandCount
+        {
+            get
+            {
+                EnsureCommandCount();
+                // ReSharper disable once PossibleInvalidOperationException
+                return commandCount.Value;
+            }
+        }
+
+        private IntVector2? startingPoint;
+
+        /// <summary>
+        /// Starting point of the cleaner navigation
+        /// </summary>
+        public IntVector2 StartingPoint
+        {
+            get
+            {
+                EnsureCommandCount();
+                EnsureStartingPoint();
+                // ReSharper disable once PossibleInvalidOperationException
+                return startingPoint.Value;
+            }
+        }
+
+        private int movesRead;
+
+        /// <summary>
+        /// Moves to execute.
+        /// <remarks>This collection is yielded only once due to the nature of the reader.
+        /// In order to read it again, please re-create the parser.</remarks>
+        /// </summary>
+        public IEnumerable<IntVector2> Moves
+        {
+            get
+            {
+                EnsureCommandCount();
+                EnsureStartingPoint();
+                while (movesRead < CommandCount)
+                {
+                    yield return GetNextMove();
+                    movesRead++;
+                }
+            }
+        }
+
+        private IntVector2 GetNextMove()
+        {
+            var parts = reader.ReadLine().Split(' ');
+            var (x, y) = GetVectorFromDirection(parts[0]);
+            if (int.TryParse(parts[1], out int distance))
+            {
+                return new IntVector2(x * distance, y * distance);
+            }
+            
+            throw new FormatException("The move is malformed!");
+        }
+
+        private (int, int) GetVectorFromDirection(string part)
+        {
+            switch (part.ToLowerInvariant())
+            {
+                case "n": return (0, 1);
+                case "e": return (1, 0);
+                case "s": return (0, -1);
+                case "w": return (-1, 0);
+                default: throw new FormatException("Unable to determine the direction.");
+            }
+        }
+
+        private void EnsureStartingPoint()
+        {
+            if (startingPoint.HasValue)
+            {
+                return;
+            }
+
+            var parts = reader.ReadLine().Split(' ');
+            if (int.TryParse(parts[0], out int x) && int.TryParse(parts[1], out int y))
+            {
+                startingPoint = new IntVector2(x, y);
+            }
+            else
+            {
+                throw new FormatException("The starting point is malformed!");
+            }
+        }
+
+        private void EnsureCommandCount()
+        {
+            if (commandCount.HasValue)
+            {
+                return;
+            }
+
+            if (int.TryParse(reader.ReadLine(), out int resultNumber))
+            {
+                commandCount = resultNumber;
+            }
+            else
+            {
+                throw new FormatException("The command count is malformed!");
+            }
+        }
+    }
+}
---
+++ Cint.RobotCleaner.Tests/InputParserTests.cs
@@ -0,0 +1,95 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text;
+using Cint.RobotCleaner.Client;
+using Cint.RobotCleaner.Core.Impl;
+using Microsoft.VisualBasic;
+using NUnit.Framework;
+
+namespace Cint.RobotCleaner.Tests
+{
+    public class InputParserTests
+    {
+
+        private string sampleInput = "5\n10 20\nE 2\nN 1\nW 5\nS 4\nW 3";
+
+        private Stream GetSampleStream(string sampleData)
+        {
+            return new MemoryStream(Encoding.UTF8.GetBytes(sampleData));
+        }
+
+        private InputParser sampleParser;
+
+        private Stream sampleDataStream;
+
+        [SetUp]
+        public void Setup()
+        {
+            sampleDataStream = GetSampleStream(sampleInput);
+            sampleParser = new InputParser(sampleDataStream);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            sampleDataStream.Dispose();
+        }
+
+        [Test]
+        public void InputParser_ShouldReadCommandCount()
+        {
+            Assert.AreEqual(5, sampleParser.CommandCount);
+        }
+
+        [Test]
+        public void InputParser_ShouldNotFailWhenReadingCommandCountManyTimes()
+        {
+            Assert.DoesNotThrow(() => {
+                var noFail = sampleParser.CommandCount;
+                var alsoNoFailAsWell = sampleParser.CommandCount;
+            });
+        }
+
+        [Test]
+        public void InputParser_ShouldReturnStartingPoint()
+        {
+            Assert.AreEqual(new IntVector2(10, 20), sampleParser.StartingPoint);
+        }
+
+        [Test]
+        public void InputParser_DoesNotFailOnRandomAccesses()
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                var noFail = sampleParser.StartingPoint;
+                var alsoNoFail = sampleParser.CommandCount;
+                var noFailAsWell = sampleParser.StartingPoint;
+            });
+        }
+
+        [Test]
+        public void InputParser_YieldsOutFullMoves()
+        {
+            var result = sampleParser.Moves.ToArray();
+            CollectionAssert.AreEqual(new[]
+            {
+                new IntVector2(2, 0),
+                new IntVector2(0, 1),
+                new IntVector2(-5, 0),
+                new IntVector2(0, -4),
+                new IntVector2(-3, 0)
+            }, result);
+        }
+
+        [Test]
+        public void InputParser_MovesCanBeReadManyTimes()
+        {
+            Assert.DoesNotThrow(() =>
+            {
+                var result = sampleParser.Moves.ToArray();
+                var doesNotFail = sampleParser.Moves.ToArray();
+                var stillDoesNotFail = sampleParser.Moves.ToArray();
+            });
+        }
+    }
+}
